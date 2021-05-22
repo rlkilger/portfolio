f9a6ed2ee4dadbadb6be8f8b86f9f53b4a9280b7
rlkilger <rlkilger@gmail.com>
2021-05-21 21:28:43 -0600

    portfolio layout change

--- js/javascript.js
+++ js/javascript.js
@@ -1,60 +1,60 @@
 //assignments list
-const links = [
-  {
-    label: "Week 1 Notes - Local Storage",
-    url: "week1/index.html"
-  },
-  {
-    label: "Week 2 Notes - Programming Basics / Arrays, Logic, and Loops / Functions",
-    url: "week2/index.html"
-  },
-  {
-    label: "Week 2 Team Activity",
-    url: "week2/team-activity2/index.html"
-  },
-  {
-    label: "Week 3 Notes - Object Methods: this / Objects / Document Object Model / Events",
-    url: "week3/index.html"
-  },
-  {
-    label: "Week 3 Team Activity 1",
-    url: "week3/team-activity3/start1.html"
-  },
-  {
-    label: "Week 3 Team Activity 2",
-    url: "week3/team-activity3/start2.html"
-  },
-  {
-    label: "Week 4 Notes - Forms / Object Oriented Programming / Modern Javascript",
-    url: "week4/index.html"
-  },
-  {
-    label: "Week 4 Team Activity",
-    url: "week4/team-activity4/tictactoe.html"
-  },
-  {
-    label: "Week 5 Notes - Testing and Debugging",
-    url: "week5/index.html"
-  },
-  {
-    label: "Week 5 Team Activity",
-    url: "week5/team-activity5/index.html"
-  }
-]
+// const links = [
+//   {
+//     label: "Week 1 Notes - Local Storage",
+//     url: "week1/index.html"
+//   },
+//   {
+//     label: "Week 2 Notes - Programming Basics / Arrays, Logic, and Loops / Functions",
+//     url: "week2/index.html"
+//   },
+//   {
+//     label: "Week 2 Team Activity",
+//     url: "week2/team-activity2/index.html"
+//   },
+//   {
+//     label: "Week 3 Notes - Object Methods: this / Objects / Document Object Model / Events",
+//     url: "week3/index.html"
+//   },
+//   {
+//     label: "Week 3 Team Activity 1",
+//     url: "week3/team-activity3/start1.html"
+//   },
+//   {
+//     label: "Week 3 Team Activity 2",
+//     url: "week3/team-activity3/start2.html"
+//   },
+//   {
+//     label: "Week 4 Notes - Forms / Object Oriented Programming / Modern Javascript",
+//     url: "week4/index.html"
+//   },
+//   {
+//     label: "Week 4 Team Activity",
+//     url: "week4/team-activity4/tictactoe.html"
+//   },
+//   {
+//     label: "Week 5 Notes - Testing and Debugging",
+//     url: "week5/index.html"
+//   },
+//   {
+//     label: "Week 5 Team Activity",
+//     url: "week5/team-activity5/index.html"
+//   }
+// ]
 
-const list = document.querySelector(".main-list");
+// const list = document.querySelector(".main-list");
 
-links.forEach(link => {
-  let li = document.createElement("li");
+// links.forEach(link => {
+//   let li = document.createElement("li");
 
-  let a = document.createElement("a");
-  a.setAttribute("href", link.url);
-  a.setAttribute("target", `_blank`);
-  a.innerHTML = link.label;
+//   let a = document.createElement("a");
+//   a.setAttribute("href", link.url);
+//   a.setAttribute("target", `_blank`);
+//   a.innerHTML = link.label;
 
-  list.append(li);
-  li.append(a);
-})
+//   list.append(li);
+//   li.append(a);
+// })
 
 
 //current year
